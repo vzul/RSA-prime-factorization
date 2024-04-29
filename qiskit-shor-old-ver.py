304abe275c802f513c8bc09c67b8a270eeeb1f56
Viviana Zuloaga <71192680+vzul@users.noreply.github.com>
2024-04-29 17:57:28 +0200

    Create qiskit-shor-old-ver.py

---
+++ qiskit-shor-old-ver.py
@@ -0,0 +1,14 @@
+from qiskit.aqua.algorithms import Shor
+from qiskit.aqua import QuantumInstance
+from qiskit import Aer
+
+key = 21 
+base = 2
+
+backend = Aer.get.backend('qasm_simulator')
+
+qi = QuantumInstance(backend=backend, shots = 1024)
+
+shors = Shor(N=key, a=base, quantum_instance = qi)
+results = shors.run
+print(results['factors])
